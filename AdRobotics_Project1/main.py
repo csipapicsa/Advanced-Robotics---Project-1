ddc018592565d55a02dd3ee24b0e88affbe18373
hefr2019 <58249848+hefr2019@users.noreply.github.com>
2024-08-29 11:54:20 +0200

    Update main.py with sensor + driver

--- AdRobotics_Project1/main.py
+++ AdRobotics_Project1/main.py
@@ -17,47 +17,41 @@ ev3 = EV3Brick()
 motorA = Motor(Port.A)
 motorB = Motor(Port.B)
 
+driver = DriveBase(left_motor=motorA, left_motor=motorB, wheel_diameter=40, axle_track=130)
+
 #Sensor
-sensorA = Motor(Port.1)
-sensorB = Motor(Port.2)
+sensorA = ColorSensor(Port.S1)
+sensorB = ColorSensor(Port.S2)
 
 
 # Write your program here.
 #Play beep sound
 ev3.speaker.beep() 
 
-target_angle = 1500 #180 degrees
-motorA.run_angle(speed=-600, rotation_angle=target_angle, wait=True)
+for i in range(6):
+    target_angle = 150
+    if (sensorA.reflection() < 10): # on the black line with sensor A
+        motorA.run_angle(speed=-600, rotation_angle=target_angle, wait=True)
+    
+    elif (sensorB.reflection() < 10): # on the black line with sensor A
+        motorB.run_angle(speed=-600, rotation_angle=target_angle, wait=True)
+    
+    else: # continue straight
+        motorA.run_time(speed=-600, time=1000, then=Stop.HOLD, wait=False)
+        motorB.run_time(speed=-600, time=1000, then=Stop.HOLD, wait=True)
 
-#Play beep sound
-ev3.speaker.beep() 
+
+# #Play beep sound
+# ev3.speaker.beep() 
 
 # #going straight for a certain time = 22 cm
-for i in range(7):
-    motorA.run_time(speed=-600, time=1000, then=Stop.HOLD, wait=False)
-    motorB.run_time(speed=-600, time=1000, then=Stop.HOLD, wait=True)
-    # motorA.stop()
-    # motorB.stop()
-    # Play another beep sound.
-    # ev3.speaker.beep(1000, 500)
+# for i in range(1):
+#     motorA.run_time(speed=-600, time=1000, then=Stop.HOLD, wait=False)
+#     motorB.run_time(speed=-600, time=1000, then=Stop.HOLD, wait=True)
+#     # motorA.stop()
+#     # motorB.stop()
+#     # Play another beep sound.
+#     # ev3.speaker.beep(1000, 500)
 
-# Play another beep sound.
-ev3.speaker.beep(1000, 500)
- 
-target_angle = 600 #90 degrees
-motorB.run_angle(speed=-300, rotation_angle=target_angle, wait=True)
-
-# motorA.run_target(speed=600, target_angle=target_angle, wait=False)
-# motorB.run_target(speed=500, target_angle=target_angle, wait=False)
-
-# Play another beep sound.
-ev3.speaker.beep(1000, 500)
-
-#going straight for a certain time = 22 cm
-motorA.run_time(speed=-700, time=1000, then=Stop.HOLD, wait=False)
-motorB.run_time(speed=-700, time=1000, then=Stop.HOLD, wait=True)
-
-ev3.speaker.beep(1000, 500)
-
-# target_angle = 20 #90 degrees
-# motorA.run_angle(speed=-300, rotation_angle=target_angle, wait=True)
+# # Play another beep sound.
+# ev3.speaker.beep(1000, 500)
